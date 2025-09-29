02ec7a51872a1a5f890eee846a584704b7e09469
DavidMaxvn <162657738+DavidMaxvn@users.noreply.github.com>
2025-09-29 12:40:36 +0700

    New change!

--- BackEnd/ConnectionDB/DB_driver.php
+++ BackEnd/ConnectionDB/DB_driver.php
@@ -6,7 +6,7 @@ class DB_driver
   // link : https://scotch.io/@phalconVee/using-mysql-on-heroku
     // Biến lưu trữ kết nối
     public $__conn,
-        $host = 'localhost',
+        $host = 'localhost:3307',
         $DbName = 'web2',
         $user = 'root',
         $pass = '';
