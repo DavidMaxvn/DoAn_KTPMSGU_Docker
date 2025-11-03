31a38a6b8ade13ccc26e37ca7ce03df27b630bee
DavidMaxvn <162657738+DavidMaxvn@users.noreply.github.com>
2025-11-03 21:15:18 +0700

    Add helper script for docker compose

--- AppView/Controllers/TrangChuController.cs
+++ AppView/Controllers/TrangChuController.cs
@@ -1,6 +1,7 @@
 ﻿using AppData.ViewModels;
 using AppData.ViewModels.BanOffline;
 using AppData.ViewModels.SanPham;
+using AppView.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -11,8 +12,7 @@ namespace AppView.Controllers
         private readonly HttpClient _httpClient;
         public TrangChuController()
         {
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("https://localhost:7095/api/");
+            _httpClient = ApiClientFactory.CreateClient();
         }
         public async Task<IActionResult> Index()
         {
