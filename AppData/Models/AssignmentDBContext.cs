007a8195f3d220c72f5f70307b9d812864c2b433
DavidMaxvn <3122411174@sv.sgu.edu.vn>
2025-11-11 00:29:25 +0700

    New changes!

--- AppData/Models/AssignmentDBContext.cs
+++ AppData/Models/AssignmentDBContext.cs
@@ -32,7 +32,7 @@ namespace AppData.Models
         public DbSet<Anh> Anhs { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=172.31.240.1,1433;Database=AppBanQuanAoThoiTrangNam;User ID=sa;Password=123456;TrustServerCertificate=True;Encrypt=False;MultipleActiveResultSets=True");
+            optionsBuilder.UseSqlServer(@"Data Source=172.24.224.1,1433;Database=AppBanQuanAoThoiTrangNam;User ID=sa;Password=123456;TrustServerCertificate=True;Encrypt=False;MultipleActiveResultSets=True");
         }
         //THUYNHU\SQLEXPRESS
         //DESKTOP-UOIH77U\SQLEXPRESS
